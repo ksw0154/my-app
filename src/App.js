d2ec47f3fa5206014b1d34c6f66bae37e28a4501
ksw0154 <rlatkddnjs44@naver.com>
2021-12-14 18:13:03 +0900

    컴포넌트 스타일링

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import SassComponent from "./ComponentStyling/SassComponent";
+import CSSModule from "./ComponentStyling/CSSModule";
 
 class App extends Component {
   render() {
     return (
       <div>
         <SassComponent />
+        <CSSModule />
       </div>
     );
   }
